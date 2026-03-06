babcfb0ccce463b37dae67ed2f86a9f0f1203b86
Nickson046 <118267383+Nickson046@users.noreply.github.com>
2026-03-06 14:38:29 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const checkboxes = document.querySelectorAll(".progress");
+const progressText = document.getElementById("progressText");
+
+checkboxes.forEach(box => {
+
+box.addEventListener("change", updateProgress);
+
+});
+
+function updateProgress(){
+
+let total = checkboxes.length;
+let checked = 0;
+
+checkboxes.forEach(box => {
+if(box.checked){
+checked++;
+}
+});
+
+let percent = Math.round((checked/total)*100);
+
+progressText.textContent = "Progress: " + percent + "%";
+
+}
